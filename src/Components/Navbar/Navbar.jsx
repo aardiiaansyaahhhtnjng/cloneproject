84af6057ed32b58de6e80787d814a847e4152455
rexrothctrlxid <rexrothiot@gmail.com>
2024-04-28 23:26:41 +0700

    punya orang

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+// Navbar with conditional rendering
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#/"><b>React Gh-Pages</b></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        {
+                            
+                            window.location.href === 'https://aardiiaansyaahhhtnjng.github.io/cloneproject/#' ?
+                                <>
+                                    <li className="nav-item">
+                                        <a className="nav-link active" aria-current="page" href="#/"><b>Home</b></a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#/login">Login</a>
+                                    </li>
+                                </>
+                            :
+                            (
+                                window.location.href === 'https://aardiiaansyaahhhtnjng.github.io/cloneproject/#/login' ? 
+                                    <>
+                                        <li className="nav-item">
+                                            <a className="nav-link" aria-current="page" href="#/">Home</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link active" href="#/login"><b>Login</b></a>
+                                        </li>
+                                    </>
+                                :
+                                    <>
+                                        <li className="nav-item">
+                                            <a className="nav-link" aria-current="page" href="#/">Home</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link" href="#/login">Login</a>
+                                        </li>
+                                    </>
+                            )
+                        }
+                    </ul>
+                </div>
+            </div>
+      </nav>
+    )
+}
+
+export default Navbar;
